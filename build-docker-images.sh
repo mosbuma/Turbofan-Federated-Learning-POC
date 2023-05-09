0f11603284e648912c7905c063470373c0ab525b
Marc Buma <mosbuma@bumos.nl>
2023-05-10 01:29:24 +0200

    build amdex_server

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -8,4 +8,5 @@ docker build -t pysyft-notebook ./pysyft-notebook
 
 # docker build -t syft-base ./docker_syft_base
 docker build -t turbofan-federated-trainer ./federated_trainer
+docker build -t amdex_server ./amdex_server
 
