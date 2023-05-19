6a6d08bf5f5c6cfd4b8a5064329473882afc361c
Marc Buma <mosbuma@bumos.nl>
2023-05-19 16:45:45 +0200

    various edits to the FL environment - change ports to allow it to run next to the DTV - partial implement new API (can authenticate and request accountinfo / version

--- notebooks/amdex.py
+++ notebooks/amdex.py
@@ -1,147 +1,187 @@
 import requests
-import time
 import json
+import time
 
-BASE_URL = "https://develop.amdex.dev"
+class AMdEX:
+    def __init__(self, base_url="https://develop.amdex.dev"):
+        self.base_url = base_url
+        self._auth = None
 
+    def getAccountInfo(self):
+        return self._auth
 
-# class BearerAuth(requests.auth.AuthBase):
-#     """Bearer Token Authentication for Requests"""
+    def authenticate(self, name, password_hash):
+        try:
+            print("got base url", self.base_url)
+            result = requests.post(
+                f"{self.base_url}/api/auth",
+                headers={"Accept": "application/json"},
+                json=json.dumps({"name": name, "password_hash": password_hash}),
+            )
+            # result.raise_for_status()
+            self._auth = result.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Authentication failed: {e}")
 
-#     def __init__(self, token):
-#         self.token = token
-
-#     def __call__(self, r):
-#         r.headers["Authorization"] = f"Bearer {self.token}"
-#         print(f"Request headers: {r.headers}")
-#         return r
-
-
-# def make_rest_call(url, token):
-#     """Make a REST API call to a given URL using Bearer Token Authentication"""
-
-#     headers = {"Accept": "application/json"}
-#     response = requests.post(url, headers=headers, auth=BearerAuth(token))
-
-#     if response.status_code == 200:
-#         return response.json()
-#     else:
-#         print("got response status code ", response.status_code)
-#         return None
-
-
-def fetchAuth(name, password_hash):
-    try:
-        url = f"{BASE_URL}/api/auth"
-        jsonobj = {
-            "name": name,
-            "password_hash": password_hash,
-        }
-
-        print("fetchAuth - url:" + url, flush=True)
-        print("fetchAuth - password_hash:" + password_hash, flush=True)
-        print("fetchAuth - json:" + json.dumps(jsonobj), flush=True)
-
-        headers = {"Accept": "application/json"}
-        result = requests.post(
-            url,
-            headers=headers,
-            json=json.dumps(jsonobj),
-        )
-        result.raise_for_status()
-        return result.json()
-    except requests.exceptions.RequestException as e:
-        print("Error during fetchAuth:", e)
-        return None
-
-
-def fetchJobs(account_uuid, bearer_token, body={}):
-    try:
-        url = f"{BASE_URL}/api/authenticated/jobs/{account_uuid}"
-        method = "GET" if not body else "POST"
-        headers = {
-            "Authorization": f"Bearer {bearer_token}",
-            "origin": BASE_URL,
-        }
-        result = requests.request(method, url, headers=headers, json=body)
-        result.raise_for_status()
-        return result.json()
-    except requests.exceptions.RequestException as e:
-        print("Error during fetchJobs:", e)
-        return None
-
-
-def pollJobStatus(
-    account_uuid,
-    job_uuid,
-    bearer_token,
-    maxTotalInterval=5 * 1000,
-    initialInterval=100,
-    intervalIncrease=200,
-):
-    try:
-        totalInterval = 0
-        interval = initialInterval
-
-        while True:
-            jobs = fetchJobs(account_uuid, bearer_token)
-            job = next((job for job in jobs if job["job_uuid"] == job_uuid), None)
-
-            if job:
-                if job["status"] == "OK":
-                    return True
-                if job["status"] == "FAILED":
-                    return False
-
-            if totalInterval >= maxTotalInterval:
+    @property
+    def version(self):
+        def get():
+            try:
+                result = requests.get(f"{self.base_url}/api/version")
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to retrieve version info: {e}")
                 return None
 
-            totalInterval += interval
-            time.sleep(interval / 1000)
-            interval += intervalIncrease
+        return {"get": get}
 
-        return None
-    except Exception as e:
-        print("Error during pollJobStatus:", e)
-        return None
+    @property
+    def dataviews(self):
+        def get():
+            try:
+                result = requests.get(
+                    f"{self.base_url}/api/authenticated/dataviews",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to retrieve dataviews: {e}")
+                return None
 
+        def post(body={}):
+            try:
+                result = requests.post(
+                    f"{self.base_url}/api/authenticated/dataviews",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                    json=json.dumps(body),
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to create dataview: {e}")
+                return None
 
-def fetchAuditlogs(account_uuid, job_uuid, bearer_token, body={}):
-    try:
-        url = f"{BASE_URL}/api/authenticated/auditlogs/{account_uuid}/{job_uuid}"
-        method = "GET" if not body else "POST"
-        headers = {
-            "Authorization": f"Bearer {bearer_token}",
-            "origin": BASE_URL,
-        }
-        result = requests.request(method, url, headers=headers, json=body)
-        result.raise_for_status()
-        return result.json()
-    except requests.exceptions.RequestException as e:
-        print("Error during fetchAuditlogs:", e)
-        return None
+        def delete(dataview_uuid):
+            try:
+                result = requests.delete(
+                    f"{self.base_url}/api/authenticated/dataviews/{dataview_uuid}",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to delete dataview: {e}")
+                return None
 
+        return {"get": get, "post": post, "delete": delete}
 
-def fetchDataviews(account_uuid, bearer_token, method="GET", body={}):
-    try:
-        url = f"{BASE_URL}/api/authenticated/dataviews"
-        headers = {
-            "Authorization": f"Bearer {bearer_token}",
-            "origin": BASE_URL,
-        }
-        result = requests.request(method, url, headers=headers, json=body)
-        result.raise_for_status()
-        return result.json()
-    except requests.exceptions.RequestException as e:
-        print("Error during fetchDataviews:", e)
-        return None
+    @property
+    def policies(self):
+        def get():
+            try:
+                result = requests.get(
+                    f"{self.base_url}/api/authenticated/policies",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to retrieve policies: {e}")
+                return None
 
+        def post(body={}):
+            try:
+                result = requests.post(
+                    f"{self.base_url}/api/authenticated/policies",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                    json=json.dumps(body),
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to create policy: {e}")
+                return None
 
-def fetchApi(api_function):
-    try:
-        result = requests.get(f"{BASE_URL}/api/{api_function}")
-        result.raise_for_status()
-        return result.json()
-    except requests.exceptions.RequestException as e:
-        print("Error during fetchApi:", e)
-        return None
+        def delete(policy_uuid):
+            try:
+                result = requests.delete(
+                    f"{self.base_url}/api/authenticated/policies/{policy_uuid}",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to delete policy: {e}")
+                return None
+
+        return {"get": get, "post": post, "delete": delete}
+
+    @property
+    def jobs(self):
+        def get():
+            try:
+                result = requests.get(
+                    f"{self.base_url}/api/authenticated/jobs/{self._auth['account_uuid']}",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to retrieve jobs: {e}")
+                return None
+
+        def post(body={}):
+            try:
+                result = requests.post(
+                    f"{self.base_url}/api/authenticated/jobs/{self._auth['account_uuid']}",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                    json=json.dumps(body),
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to create job: {e}")
+                return None
+
+        def delete():
+            try:
+                result = requests.delete(
+                    f"{self.base_url}/api/authenticated/jobs/{self._auth['account_uuid']}",
+                    headers={
+                        "Authorization": f"Bearer {self._auth['bearer_token']}",
+                        "origin": self.base_url,
+                    },
+                )
+                result.raise_for_status()
+                return result.json()
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to delete jobs: {e}")
+                return None
+
+        return {"get": get, "post": post, "delete": delete}
