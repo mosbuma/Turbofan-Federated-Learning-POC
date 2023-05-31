72a3418fd31e70196a61bbb2de9179d2d5db1862
Marc Buma <mosbuma@bumos.nl>
2023-05-31 12:52:19 +0200

    enable usage from VS code

--- pysyft-notebook/entrypoint.sh
+++ pysyft-notebook/entrypoint.sh
@@ -10,4 +10,6 @@ fi
 
 cd $WORKSPACE
 #jupyter notebook --ip=`cat /etc/hosts |tail -n 1|cut -f 1` --allow-root
-jupyter lab -NotebookApp.token='amdex_rulez' --ip=`cat /etc/hosts |tail -n 1|cut -f 1` --allow-root
+jupyter lab -NotebookApp.token='amdex_rulez' --ip=`cat /etc/hosts --NotebookApp.allow_origin_pat=https://.*vscode-cdn\.net |tail -n 1|cut -f 1` --allow-root
+# jupyter lab -NotebookApp.token='amdex_rulez' --ip=`cat /etc/hosts --allow-root --NotebookApp.allow_origin_pat=https://.*vscode-cdn\.net |tail -n 1|cut -f 1` 
+# jupyter lab -NotebookApp.token='amdex_rulez' --ip=`cat /etc/hosts --allow-root |tail -n 1|cut -f 1` 
